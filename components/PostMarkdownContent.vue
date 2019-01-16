8a0cbf844104c3478693bda4bbb5e2b9b73105dd
Callum Flack <callumflack@gmail.com>
2019-01-16 10:55:38 +1000

    update: turn off image resize in markdown

--- components/PostMarkdownContent.vue
+++ components/PostMarkdownContent.vue
@@ -11,7 +11,8 @@ export default {
     parsed() {
       // Parse markdown using "marked" and add a parameter for the
       // Storyblok Image Service: https://www.storyblok.com/docs/image-service
-      return markdown(this.content, "700x0");
+      // return markdown(this.content, "700x0");
+      return markdown(this.content);
     }
   }
 };
